6a2117f41959d065b7d3914c18476b3658f1d937
Thiago Silva <thicsilva@yahoo.com.br>
2022-06-07 20:54:50 -0300

    removido classes em desuso

--- TestService/TestService/Program.cs
+++ TestService/TestService/Program.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading;
-using Microsoft.Extensions.Options;
 
 namespace TestService
 {
